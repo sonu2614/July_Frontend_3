2356eed6e305f5b13b4bcab75a734a102449efe4
Sonu <chaudharysonu2614@gmail.com>
2023-07-24 15:37:32 +0530

    update

--- THIS_keyword/script.js
+++ THIS_keyword/script.js
@@ -1,126 +1,208 @@
 // Today's topic is THIS keyword
 
-// console.log(this);
-// function test(){
-//     console.log(this)
-// }
+/*
+console.log(this);
+function test(){
+    console.log('test');
+}
 
-// window.test();
-// test();
+window.test();
+test();
+*/
 
-// implicit binding is applied when you invoke a functin in an object using (.) notation .THIS keyword in such case will point towards the object.
+//IMPLICIT BINDING:-->
+// implicit binding is applied when you invoke a functin
+// in an object using (.) notation .THIS keyword 
+//in such case will point towards the object.
 
-// THIS keyword is used to refer something like an object . what is value of THIS keyword will depend on the context we are in.
+// THIS keyword is used to refer something like an object.
+// what is value of THIS keyword will depend on the 
+//context we are in.
 
-// let obj ={
-//     a:10,
-//     test(){
-//         console.log(this)
-//     },
-// };
 
-// obj.test();
+/*
+console.log(this);
+function test(){
+    console.log(this)
+}
+
+window.test();
+test();
+*/ 
+
+
+/*
+let obj ={
+    a:10,
+    test(){
+        console.log(this)
+        //we are printing THIS keyword
+    },
+};
+
+obj.test();
+*/
+
+/*
+let obj ={
+    a:10,
+    test(){
+        let a=5;
+        let b=10;
+        console.log(a+b);
+        console.log(this)
+    },
+};
+
+obj.test();
+*/
 
 // //test();
 
-// let obj ={
-//     a:10,
-//     test(){
-//         let a=5;
-//         let b=10;
-//         console.log(a+b)
-//         console.log(this)
-//     },
-// };
 
-// obj.test();
+/*
+this.a=5;
+console.log(a)          //>> 5
+*/
 
+/*
+this.a=5;
+function getParam(){
+    console.log(this.a)
+}
+getParam();         //>> 5
+*/
 
-// this.a=5;
-// console.log(a)
+/*
+this.a=5;
+const getParam=() => {
+    console.log(this.a);
+}
+getParam();         //>>5
+*/
 
-// this.a=5;
-// function getParam(){
-//     console.log(this.a)
-// }
-// getParam();
+/*
+this.name='pc';
+let user={
+    name:'Prikshit',
+    age:23,
+    getDetails(){
+        console.log(this.name);
+    }
+}
+user.getDetails();          //>>Prikshit
+*/
 
-// this.a=5;
-// const getParam=() => {
-//     console.log(this.a);
-// }
-// getParam();
+/*
+this.name='pc';
+let user={
+    // nam:'Prikshit',
+    age:22,
+    getDetails(){
+        console.log(this.name);
+    }
+}
+user.getDetails();          //>>undefined
+*/
 
+/*
+let user={
+    nam:'Prikshit',
+    age:22,
+    childObj:{
+        newName:'pc',
+        getDetails(){
+            console.log(this.newName, 'and', this.nam);
+        }
+    }
+}
+user.childObj.getDetails();         //>>pc and undefined
+*/
 
-// this.name='pc';
-// let user={
-//     name:'Prikshit',
-//     age:23,
-//     getDetails(){
-//         console.log(this.name);
-//     }
-// }
-// user.getDetails();
+/*
+let user={
+    nam:'Prikshit',
+    age:22,
+    getDetails:() => {
+        console.log(this);
+    }
+}
+user.getDetails();          //Window {window: Window, self: Window, document: document, name: 'pc', location: Location, …}
+*/
 
-// let user={
-//     name:'Prikshit',
-//     age:23,
-//     childObj:{
-//         newName:'pc',
-//         getDetails(){
-//             console.log(this.newName, 'and', this.name);
-//         }
-//     }
-// }
-// user.childObj.getDetails();
+/*
+let user={
+    name:'Prikshit',
+    age:23,
+    getDetails() {
+        //the this inside my arrow fn
+        //will point towards this inside
+        //outer normal fn
+        console.log(this);
+        const nestedArrow = () => console.log(this);
+        nestedArrow();
+    }
+}
+user.getDetails();      //>{name: 'Prikshit', age: 23, getDetails: ƒ}
+                        //{name: 'Prikshit', age: 23, getDetails: ƒ}
+*/
 
-// let user={
-//     name:'Prikshit',
-//     age:23,
-//     getDetails:() => {
-//         console.log(this );
-//     }
-// }
-// user.getDetails();
+/*
+const user = {
+    firstName:'prakshit',
+    getName(){
+        const firstName='pc';
+        return this.firstName;
+    }
+}
+console.log(user.getName());            //>>prakshit
+*/
 
-//  
+/*
+const user={
+    nam:'pc',
+    logMessage(){
+        console.log(this.nam);
+    }
+}
+setTimeout(user.logMessage,1000)            //>>undefined
+*/
 
+/*
+const user={
+    nam:'pc',
+    logMessage(){
+        console.log('test');
+        console.log(this.nam);
+    }
+}
+user.logMessage();                 //>>test
+                                  //>>pc
+*/
 
-// let user={
-//     name:'Prikshit',
-//     age:23,
-//     getDetails() {
-//         console.log(this);
-//         const nestedArrow = () => console.log(this);
-//         nestedArrow();
-//     }
-// }
-// user.getDetails();
+/*
+const user={
+    nam:'pc',
+    logMessage(){
+        console.log('test');
+        console.log(this.nam);
+    }
+}
+setTimeout(user.logMessage,1000)        //>>test
+                                        //>>undefined
+*/
 
-// const user = {
-//     firstName:'prakshit',
-//     getName(){
-//         const firstName='pc';
-//         return this.firstName;
-//     }
-// }
-// console.log(user.getName());
+/*
+const user={
+    nam:'pc',
+    logMessage(){
+        console.log('test');
+        console.log(this.nam);
+    }
+}
+setTimeout(() => {
+    user.logMessage();
+},1000)                     //>>test
+                            //>>pc
+*/
 
-// const user={
-//     nam:'pc',
-//     logMessage(){
-//         console.log(this.nam);
-//     }
-// }
-// setTimeout(user.logMessage,1000)
-
-// const user={
-//     nam:'pc',
-//     logMessage(){
-//         console.log('test');
-//         console.log(this.nam);
-//     }
-// }
-// setTimeout(() => {
-//     user.logMessage();
-// },1000)
